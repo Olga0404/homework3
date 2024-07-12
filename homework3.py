63fa43914b646450c8e837c05bd5b67bd5aa8f6d
Olga0404 <o.slesorova@gmail.com>
2024-07-12 23:48:04 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Olga'
+print ('Name: ', name, type (name))
+age = 38
+print ('Age', age,type (age))
+age=age + 5
+print ('New age', age,type(age))
+is_student = True
+print ('Is_student', is_student, type (is_student))
